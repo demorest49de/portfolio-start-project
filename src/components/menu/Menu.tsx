2c7b2039e967aeff29daf7f934c9db592931b89b
Shevchenko Andrey <demorest49de@gmail.com>
2024-01-18 11:58:41 +0300

    Changes Menu.tsx

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,15 +1,36 @@
+import styled from "styled-components";
 
 export const Menu = (props: { menuItems: string[] }) => {
     return (
         <ul>
             {
                 props.menuItems.map((item, index) => {
-                    return <li key={index}>
-                        <a href="#" aria-label={item}>{item}
-                        </a></li>
+                    return (
+                        <ListItem key={index}>
+                            <Link href={`#${item}`} aria-label={item}>
+                                <Mask>
+                                    {item}
+                                </Mask>
+                                <Mask>
+                                    {item}
+                                </Mask>
+                            </Link>
+                        </ListItem>
+                    )
                 })
             }
         </ul>
     )
 }
 
+const ListItem = styled.li`
+    
+`
+
+const Link = styled.a`
+
+`
+
+const Mask = styled.span`
+
+`
