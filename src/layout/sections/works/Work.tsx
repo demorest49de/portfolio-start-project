41be1fa0974cfda211da3c45a4ad8ad49953116c
Shevchenko Andrey <demorest49de@gmail.com>
2024-01-18 19:56:01 +0300

    Changes Work.tsx

--- src/layout/sections/works/Work.tsx
+++ src/layout/sections/works/Work.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import FlexWrapper from "../../../components/flexWrapper/FlexWrapper";
+import theme from "../../../styles/Theme.Styled";
 
 
 type WorkPropsType = {
@@ -10,32 +12,77 @@ type WorkPropsType = {
 export const Work = (props: WorkPropsType) => {
     return (
         <StyledWorkCards>
-            <Image src={props.src} alt=""/>
-            <Title>{props.title}</Title>
-            <Text>{props.text}</Text>
-            <Link href={'#'}></Link>
-            <Link href={'#'}></Link>
+            <ImageWrapper>
+                <Image src={props.src} alt=""/>
+            </ImageWrapper>
+            <StyledAboutSubblock>
+                <Title>{props.title}</Title>
+                <Text>{props.text}</Text>
+                <FlexWrapper gap={'20px'}>
+                    <Link href={'#'}>demo</Link>
+                    <Link href={'#'}>code</Link>
+                </FlexWrapper>
+            </StyledAboutSubblock>
         </StyledWorkCards>
     );
 };
 
+const ImageWrapper = styled.div`
+  position: relative;
+  max-height: 260px;
+  margin-bottom: 25px;
+
+  &:hover {
+    transition: all .3s ease-in-out;
+
+    &::before {
+      position: absolute;
+      content: '';
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+
+
+      background-color: rgba(0, 0, 0, 0.3);
+      backdrop-filter: blur(4px);
+    }
+
+  }
+
+`
+
 const StyledWorkCards = styled.div`
-    max-width: 540px;
-    max-height: 433px;
+  max-width: 540px;
+  width: 100%;
+  max-height: fit-content;
+  background-color: ${theme.backgroundColor.secondary};
 `
 
 const Image = styled.img`
-
+  max-width: 540px;
+  width: 100%;
+  max-height: inherit;
+  object-fit: cover;
+  object-position: left center;
 `
 
 const Title = styled.h3`
-
+  margin-bottom: 14px;
 `
 
 const Text = styled.p`
-
+  margin-bottom: 19px;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
 `
 
 const Link = styled.a`
+  text-transform: uppercase;
+`
 
+const StyledAboutSubblock = styled.div`
+  padding: 0 20px 30px 20px;
 `
